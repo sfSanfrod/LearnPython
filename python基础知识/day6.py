0e1ee59abab1af5e958869f009ec03a97ffa1f91
sfSanfrod <985758828@qq.com>
2018-07-11 20:29:34 +0800

    add

---
+++ python基础知识/day6.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#encoding=utf-8
+
+__author__ = '小白龙'
+
+"""
+ElementTree是Python提供解析xml的标准库，ElementTree中每个节点（即Element）具有如下属性：
+
+tag： string对象，标识该元素类型
+
+attrib：dictionnary对象，标识该元素属性
+
+text：string对象，标识该元素的文本
+
+tail：string对象，标识该元素可选的尾字符串
+
+child elements： 标识子节点
+"""
