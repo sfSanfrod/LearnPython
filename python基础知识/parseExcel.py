dffc80befaaf1d733b3d6d516a37c2421b7c18a9
sfSanfrod <985758828@qq.com>
2018-07-23 22:34:13 +0800

    update excel

--- python基础知识/parseExcel.py
+++ python基础知识/parseExcel.py
@@ -6,10 +6,26 @@
 __author__ = "小白龙"
 
 from openpyxl import Workbook
+from openpyxl import load_workbook
+import datetime
+
+def writeExcl():
+    wb = Workbook()
+    sheet1 = wb.active
+    sheet1['A1'] = "书名"
+    sheet1.append([1, 2, 4])
+    sheet1['B1'] = "时间"
+    sheet1.append([datetime.datetime.now()])
+
+    wb.save("写入excel示例.xlsx")
+
+def readExcel(path):
+    print("读取Excel：",path)
+    wb = load_workbook(path)
+    sheets = wb.get_sheet_names()
+    print(type(sheets))
+    print("工作簿：",sheets)
 
 if __name__ == "__main__":
-    wb = Workbook()
-    sheet = wb.active
-    sheet['A1'] = "书名"
-
-    wb.save("简单excel写示例.xlsx")
+    writeExcl()
+    readExcel("读取Excel示例.xlsx")
